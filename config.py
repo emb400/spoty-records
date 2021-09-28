8ef3edc79bb65b48ae71074bb01846790326c837
emb400 <91533404+emb400@users.noreply.github.com>
2021-09-28 11:20:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ Created on Wed Jan 29 10:29:55 2020
 
 #insert local parameters
 
-username = 'your-spotify-username'
-client_id ='your-client-id'
-client_secret = 'your-client-secret'
+username = '12zk5jwhnzvjt8zo3vls9s55j'
+client_id ='6c62c5755fbe49eda72a28031ff3a59e'
+client_secret = 'fd72baaa8a3841d5be988891bd78044b'
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-read-recently-played'
